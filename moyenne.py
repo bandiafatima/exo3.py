0a8dbe39fd726d63cafe8ddc318796a4b2c007c9
bandiafatima <49444469+bandiafatima@users.noreply.github.com>
2019-04-12 08:52:52 +0000

    Add files via upload

---
+++ moyenne.py
@@ -0,0 +1,7 @@
+# calcul de la moyenne
+
+x = input("entrer la premiere valeur")
+y = input("entrer la deuxieme valeur")
+somme = int(x) + int(y)
+moyenne = int(somme) / 2
+print("la moyenne est de:", moyenne)
